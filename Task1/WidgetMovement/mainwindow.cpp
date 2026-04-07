efc1c73a40e07e3988c26c6adaba5e4065887de6
EugeniusMiroshnichenko <zhnya.miroshnichenko@mail.ru>
2026-04-07 21:54:47 +0500

    Правки mainwindows.cpp

--- Task1/WidgetMovement/mainwindow.cpp
+++ Task1/WidgetMovement/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 #include <QRandomGenerator>
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,77 +8,47 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    createTimer = new QTimer(this);
-    moveTimer = new QTimer(this);
+    QTimer* createTimer = new QTimer(this);
 
-    QObject::connect(createTimer, &QTimer::timeout, [this]() {
+    QObject::connect(createTimer, &QTimer::timeout, [this, createTimer]() {
         QPushButton *button = new QPushButton(this);
-        button->setFixedSize(25, 25);
+        int buttonWidth = 25;
+        button->setFixedSize(buttonWidth, buttonWidth);
         button->setText("*");
         int maxX = width();
         int maxY = 100;
         if(maxX < 0) maxX = 0;
-        int randomX = QRandomGenerator::global()->generate() % (maxX - 25);
-        int randomY = QRandomGenerator::global()->generate() % (maxY - 25);
+        int randomX = QRandomGenerator::global()->generate() % (maxX - buttonWidth);
+        int randomY = QRandomGenerator::global()->generate() % (maxY - buttonWidth);
         button->move(randomX, randomY);
-        button->setProperty("lives", QRandomGenerator::global()->generate() % (3));
         button->show();
 
         QObject::connect(button, &QPushButton::clicked, [this, button]() {
-            int lives = button->property("lives").toInt();
-                        lives--;
-            if (lives <= 0)
-            {
-                int index = buttons.indexOf(button);
-                buttons.removeAt(index);
-                delete button;
-            }
-            else button->setProperty("lives", lives);
+            delete button;
         });
 
+        int speed = QRandomGenerator::global()->bounded(1, 5);
+        QTimer* moveTimer = new QTimer(button);
+        QObject::connect(moveTimer, &QTimer::timeout, [button, this, moveTimer, speed]() {
+            int current_speed = speed;
+            if(button->underMouse()) current_speed *= 2;
+            button->move(button->x(), button->y() + current_speed);
 
-        buttons.append(button);
-
-        createTimer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
-    });
-
-    QObject::connect(moveTimer, &QTimer::timeout, [this]() {
-        for(int i = buttons.size() - 1; i >= 0; i--) {
-            QPushButton *btn = buttons[i];
-
-            if (!btn) {
-                buttons.removeAt(i);
-                continue;
-            }
-
-            int speed = QRandomGenerator::global()->generate() % (5);
-
-            if(btn->underMouse()) {
-                speed *= 2;
-            }
-
-            btn->move(btn->x(), btn->y() + speed);
-
-            if(btn->y() + btn->height() >= height()) {
-                buttons.removeAt(i);
+            if(button->y() + button->height() >= this->height()) {
                 setStyleSheet("QMainWindow { background-color: red; }");
                 setWindowTitle("YOU LOOSE!");
-                delete btn;
+                delete button;
             }
-        }
-    });
+        });
+        moveTimer->start(100);
+        createTimer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
+    });  
 
     createTimer->start(500);
-    moveTimer->start(100);
 }
 
 MainWindow::~MainWindow()
 {
-    for(int i = 0; i < buttons.size(); i++) {
-        delete buttons[i];
-        buttons[i] = nullptr;
-    }
-    buttons.clear();
     delete ui;
 }
 
